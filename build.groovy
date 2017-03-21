0469bf66b61fc32a54280721a1ed15f86d4e7bf8
pc-hard <pc-hard@mail.ru>
2017-03-21 21:52:55 +0200

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,1 +1,1 @@
-cp /var/lib/jenkins/jobs/Demo2/jobs/build/workspace/hello.py /var/lib/jenkins/jobs/Demo2/jobs/deploy/workspace
+cp /var/lib/jenkins/jobs/Demo2/jobs/build/workspace/hello.py /var/lib/jenkins/jobs/Demo2/jobs/deploy/workspace/
