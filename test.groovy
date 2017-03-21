73eb4d8437de7ebc266601d8c6893910634e8110
pc-hard <pc-hard@mail.ru>
2017-03-21 19:54:48 +0200

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,1 +1,1 @@
-pylint '/var/lib/jenkins/jobs/Demo2_again/workspace/hello.py' || cp '/var/lib/jenkins/jobs/Demo2_again/workspace/hello.py' /var/lib/jenkins/jobs/Demo2/jobs/build/workspace
+pylint '/var/lib/jenkins/jobs/Demo2/workspace/hello.py' || cp '/var/lib/jenkins/jobs/Demo2/workspace/hello.py' /var/lib/jenkins/jobs/Demo2/jobs/build/workspace
