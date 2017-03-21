f4f938646fb61ac258c8bcfddbacdabcc2510c82
pc-hard <pc-hard@mail.ru>
2017-03-21 22:49:13 +0200

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,1 +1,1 @@
-pylint '/var/lib/jenkins/jobs/Demo/jobs/test/workspace/hello.py' || cp '/var/lib/jenkins/jobs/Demo/jobs/test/workspace/hello.py' /var/lib/jenkins/jobs/Demo/jobs/build/workspace/
+pylint '/var/lib/jenkins/jobs/Demo2/jobs/test/workspace/hello.py' || cp '/var/lib/jenkins/jobs/Demo2/jobs/test/workspace/hello.py' /var/lib/jenkins/jobs/Demo2/jobs/build/workspace
