8e217c2ab17d2f0cdeee31117d9c0cffe4e10122
pc-hard <pc-hard@mail.ru>
2017-03-21 19:11:33 +0200

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,1 +1,2 @@
 scp /var/lib/jenkins/jobs/Demo2/jobs/deploy/workspace/hello.py user@172.31.119.21:/var/lib/jenkins
+ssh -i /var/lib/jenkins/.ssh/id_rsa user@172.31.119.21 'sudo service uwsgi restart'
