7841bcedc2a6bf9ce2aa305efafe7f059e1b87ff
pc-hard <pc-hard@mail.ru>
2017-03-21 19:08:14 +0200

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -1,1 +1,1 @@
-scp /var/lib/jenkins/jobs/Demo2/jobs/deploy/workspace user@172.31.119.21:/var/lib/jenkins
+scp /var/lib/jenkins/jobs/Demo2/jobs/deploy/workspace/hello.py user@172.31.119.21:/var/lib/jenkins
