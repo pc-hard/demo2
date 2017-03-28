26893652bf5af8e82e772f185d8f1b3a4a7a8b6c
pc-hard <pc-hard@mail.ru>
2017-03-28 20:24:34 +0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,3 +1,4 @@
 #"echo ${jsondata.build.ipserver}"
 echo "${ipserver}"
+ping "${ipserver}"
 # cp /var/lib/jenkins/jobs/Demo/jobs/build/workspace/hello.py /var/lib/jenkins/jobs/Demo/jobs/deploy/workspace/
